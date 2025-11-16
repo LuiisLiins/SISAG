a1bc0bccfc1b1b588d4f350daf13d072e79e4c95
Wallaf Oliveira <wallaffelipe863@gmail.com>
2025-11-16 15:54:37 -0300

    front-end

---
+++ SISAG-Sistema-Integrado-de-Sa-de-e-Agendamento2/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './style.css'
+import '@flaticon/flaticon-uicons/css/all/all.css'
+
+createApp(App).use(router).mount('#app')
